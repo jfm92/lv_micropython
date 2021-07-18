82a8ba72a390cc34a2af3474f1cf9be8e01d8c5a
jfm92 <jfloresm92@gmail.com>
2021-07-18 21:16:34 +0200

    Deleting useless init packet

--- ports/microByte/boards/manifest.py
+++ ports/microByte/boards/manifest.py
@@ -3,7 +3,6 @@ freeze("$(MPY_DIR)/tools", ("upip.py", "upip_utarfile.py"))
 freeze("$(MPY_DIR)/ports/esp8266/modules", "ntptime.py")
 freeze("$(MPY_DIR)/drivers/dht", "dht.py")
 freeze("$(MPY_DIR)/drivers/onewire")
-freeze("../../../../micropython-stage/tinypicost7789/freeze")
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
 include("$(MPY_DIR)/extmod/webrepl/manifest.py")
 
